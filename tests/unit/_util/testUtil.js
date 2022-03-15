e4ef7f768c0b711dc63bd40ed1f90b6730b6fc61
Francisco Pereira <ZNacJSmJqy7YF58arS6C>
2022-03-15 00:17:34 -0300

    test: create the routes unit tests

---
+++ tests/unit/_util/testUtil.js
@@ -0,0 +1,47 @@
+import { jest } from "@jest/globals";
+
+import { Readable, Writable } from "stream";
+
+export default class TestUtil {
+  static generateReadableStream(data) {
+    return new Readable({
+      read() {
+        for (const item of data) {
+          this.push();
+        }
+
+        this.push(null);
+      },
+    });
+  }
+
+  static generateWritableStream(onData) {
+    return new Writable({
+      write(chunk, enc, cb) {
+        onData(chunk);
+        cb(null, chunk);
+      },
+    });
+  }
+
+  static defaultHandlerParams() {
+    const requestStream = TestUtil.generateReadableStream(["body of request"]);
+    const responseStream = TestUtil.generateWritableStream(() => {});
+    const data = {
+      request: Object.assign(requestStream, {
+        headers: {},
+        method: "",
+        url: "",
+      }),
+      response: Object.assign(responseStream, {
+        writeHead: jest.fn(),
+        end: jest.fn(),
+      }),
+    };
+
+    return {
+      values: () => Object.values(data),
+      ...data,
+    };
+  }
+}
